65f614e8963f19e950e2503499207835f318dc96
Vikas Pal <vikaspal9004@gmail.com>
2026-02-20 21:33:01 +0530

    message

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ app.get('/', (req, res) => {
   res.send('Backend is running');
 });
 
-app.listen(port, 'localhost', () => {
-  console.log(`✅ Server running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`✅ Server running on http://0.0.0.0:${port}`);
 });
 
